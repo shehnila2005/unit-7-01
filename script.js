bcbcd50d68afeff0d5e30ad89c115fbebeefc4a1
Shehnila Azmat <shehnila.azmat@ocsbstudent.ca>
2021-02-19 01:22:48 +0000

    a lot

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
 document.getElementById('button').addEventListener('click', randomizer)
-let  randomNumber = 0
+let randomNumber = 0
 let userGuess = 0
 
-function randomizer() {
-userGuess = document.getElementById('input')           .value
-userGuess = parseInt(userGuess)
-
-
-randomNumber = (Math.random() * 6) + 1
-randomNumber = parseInt(randomNumber)
-if (userGuess== randomNumber) {
-
+function randomizer () {
+  userGuess = document.getElementById('input').value
+  userGuess = parseInt(userGuess)
+  randomNumber = (Math.random() * 6) + 1
+  randomNumber = parseInt(randomNumber)
+  if (userGuess === randomNumber) {
   document.getElementById('answer').innerHTML = 'correct'
   }  
 }
